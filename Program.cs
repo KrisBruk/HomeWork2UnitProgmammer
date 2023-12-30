9a7720bd0d4562e5a7c9c42dfbc2e404a6ca9707
Kristina Bruk <doctorbruk@gmail.com>
2023-12-30 19:10:31 +0300

    add task3

--- Program.cs
+++ Program.cs
@@ -24,30 +24,48 @@
 
 
 //Задача 2. Определение четверти заданной координаты
-using System;
+//using System;
 
-  class Program 
-{
-      static void Main()
-       {
-          Console.Write("Введите координаты точки X и Y через пробел: "); 
+  //class Program 
+//{
+  //    static void Main()
+    //   {
+      //    Console.Write("Введите координаты точки X и Y через пробел: "); 
+        //  
+          //string[] coordinates = Console.ReadLine().Split(' ');
           
-          string[] coordinates = Console.ReadLine().Split(' ');
-          
-          int x = Convert.ToInt32(coordinates[0]);
-          int y = Convert.ToInt32(coordinates[1]);
+          //int x = Convert.ToInt32(coordinates[0]);
+          //int y = Convert.ToInt32(coordinates[1]);
 
 
-          if (x > 0 && y > 0) 
-             Console.WriteLine("1");
-            else if (x < 0 && y > 0) 
-               Console.WriteLine("2");
-            else if (x < 0 && y < 0) 
-               Console.WriteLine("3");
-            else if (x > 0 && y < 0)
-                Console.WriteLine("4");
-           else
-             Console.WriteLine("Точка находится на оси координат");
-       }
-}
+          //if (x > 0 && y > 0) 
+            // Console.WriteLine("1");
+            //else if (x < 0 && y > 0) 
+              // Console.WriteLine("2");
+            //else if (x < 0 && y < 0) 
+              // Console.WriteLine("3");
+            //else if (x > 0 && y < 0)
+              //  Console.WriteLine("4");
+           //else
+             //Console.WriteLine("Точка на оси координат");
+       //}
+//}
+
+// Задача 3. Показать наибольшую цифру числа
+
+
    
+using System;
+    class Program {
+       static void Main() {
+              Console.Write("Введите число от [10, 99]: "); 
+              
+              int number = Convert.ToInt32(Console.ReadLine());
+               
+                int first  = number / 10;
+                int second = number % 10;
+                int max = first> second ? first : second;
+              
+              Console.WriteLine(max);
+               }
+}
