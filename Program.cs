2ac69e527cfe102ea06092256efea624f54f672e
Kristina Bruk <doctorbruk@gmail.com>
2023-12-30 17:55:47 +0300

    first task done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// проверка кратности на 7 и 23
 
+//using System;
+
+//class Program
+//{
+
+
+
+  //static void Main()
+  //{
+    //Console.Write("Введите число a: ");
+    //int a = Convert.ToInt32(Console.ReadLine());
+
+    //if (a % 7 == 0 && a % 23 == 0)
+
+    //{ Console.WriteLine("да"); }
+    //else
+    //{
+      //Console.WriteLine("нет");
+    //}
+  //}
+//}
+
