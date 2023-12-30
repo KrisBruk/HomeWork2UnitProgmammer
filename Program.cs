8434d2eb8e193330cee776e815ec81d69579ecc3
Kristina Bruk <doctorbruk@gmail.com>
2023-12-30 18:05:53 +0300

    add task2

--- Program.cs
+++ Program.cs
@@ -7,18 +7,35 @@
 
 
 
-  //static void Main()
-  //{
-    //Console.Write("Введите число a: ");
-    //int a = Convert.ToInt32(Console.ReadLine());
+//static void Main()
+//{
+//Console.Write("Введите число a: ");
+//int a = Convert.ToInt32(Console.ReadLine());
 
-    //if (a % 7 == 0 && a % 23 == 0)
+//if (a % 7 == 0 && a % 23 == 0)
 
-    //{ Console.WriteLine("да"); }
-    //else
-    //{
-      //Console.WriteLine("нет");
-    //}
-  //}
+//{ Console.WriteLine("да"); }
+//else
+//{
+//Console.WriteLine("нет");
 //}
+//}
+//}
+
+
+//Задача 2. Определение четверти заданной координаты
+
+using System;
+class Program
+{
+  static void Main()
+  {
+    Console.Write("Введите координаты точки X и Y через пробел: "); string[] coordinates = Console.ReadLine().Split(' ');
+    int x = Convert.ToInt32(coordinates[0]);
+    int y = Convert.ToInt32(coordinates[1]);
+    if (x > 0 && y > 0) Console.WriteLine("1");
+    else if (x < 0 && y > 0) Console.WriteLine("2");
+    else if (x < 0 && y < 0) Console.WriteLine("3");
+    else (x > 0 && y < 0) Console.WriteLine("4");
+  }
 
